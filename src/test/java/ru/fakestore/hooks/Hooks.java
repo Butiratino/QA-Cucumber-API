bc0db04d097a89b1c008e2d3739d1ac0e5a50c26
Butiratino <troller.facer.99@gmail.com>
2026-07-24 01:09:49 +0300

    add log information before the start of each test

--- src/test/java/ru/fakestore/hooks/Hooks.java
+++ src/test/java/ru/fakestore/hooks/Hooks.java
@@ -1,21 +1,24 @@
 package ru.fakestore.hooks;
 
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import java.util.UUID;
 
 public class Hooks {
     
+    @Before
+    public void beforeTest(Scenario scenario){
+        System.out.println("Запуск сценария: " + scenario.getName());
+        System.out.println("Тег сценария: " + scenario.getSourceTagNames());
+    }
+
     @After
     public void afterScenario(Scenario scenario){
-        String reportId = UUID.randomUUID().toString().substring(0,8);
-
-        String cdnLink = "cdn.fakestore-reports.com/reports/" + reportId + "/html";
         if(scenario.isFailed()){
             System.out.println("В сценарии произошла ошибка: " + scenario.getName());
         }else{
             System.out.println("Сценарий отработал успешно: " + scenario.getName());
         }
-        System.out.println("Ссылька на отчёт: " + cdnLink);
     }
 }
